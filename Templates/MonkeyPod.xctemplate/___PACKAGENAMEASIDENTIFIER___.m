3f5bf20888fadd45d0c067102c2d3fbee6c2f82b
kshipeng <18022364+kshipeng@users.noreply.github.com>
2026-05-10 00:18:27 +0800

    Update ___PACKAGENAMEASIDENTIFIER___.m

--- Templates/MonkeyPod.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
+++ Templates/MonkeyPod.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -5,7 +5,6 @@
 //  ___PACKAGENAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 #import "___FILEBASENAME___.h"
